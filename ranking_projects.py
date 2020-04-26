891006003c260fa48b311df40e01cc3e37f7d5a8
InkasRas <petrov.andreha@gmail.com>
2020-04-26 16:59:43 +0300

    fixed error with deleted project ids

--- ranking_projects.py
+++ ranking_projects.py
@@ -3,11 +3,11 @@ from flask import Blueprint, render_template, request, redirect, url_for, abort,
 from db_session import create_session, create_coon
 from models import ranked_table, Projects
 from flask_login import current_user
-from sqlalchemy import insert
+from sqlalchemy import insert, func
 import schedule
 
 blueprint = Blueprint('ranking_projects', __name__, template_folder='templates')
-
+ADMIN='A'
 
 def add_to_already_ranked(id, rank):
     if current_user.is_authenticated:
@@ -41,8 +41,10 @@ def add():
     ans = add_to_already_ranked(int(request.args.get('pr_id')), request.args.get('rank'))
     if ans['response'] == 200:
         next_project_ans = choose_project()
+        print('went to necxt project', next_project_ans)
         if not next_project_ans:
             url = url_for('base')
+            print('Return url')
             return jsonify({'url': url})
         next_project, new_last_id = next_project_ans
         html_template = render_template('rank_project.html', project=next_project.tojson())
@@ -52,6 +54,7 @@ def add():
 
 def choose_project():
     '''this function is for returning single project to be displayed as a rated one'''
+    print('choose prjsct', current_user.is_anonymous)
     sesion = create_session()
     if current_user.is_anonymous:
         '''Unauthorized user. If he has a cookie, return the next project to cookie.
@@ -62,14 +65,24 @@ def choose_project():
         else:
             last_prjct_id = 1
     else:
-        '''TODO: Either use knn algorithm for finding best project or just iterate through user interests'''
+        '''TODO: Either use knn algorithm for finding best project. or just iterate through user interests'''
         '''Now just returns the same thing as unauthorized'''
         last_prjct_id = request.cookies.get('last_project_id', None)
+        print('last prjct id', last_prjct_id)
         if last_prjct_id:
             last_prjct_id = int(last_prjct_id) + 1
         else:
             last_prjct_id = 1
-    project = sesion.query(Projects).get(last_prjct_id)
+        print('real last prjct id', last_prjct_id)
+        project = sesion.query(Projects).get(last_prjct_id)
+        try:
+            max_id = sesion.query(func.max(Projects.id))[0][0]
+        except:
+            max_id = 2
+        while not project and last_prjct_id <= max_id:
+            last_prjct_id += 1
+            project = sesion.query(Projects).get(last_prjct_id)
+    print('This is for', last_prjct_id, project)
     if not project:
         return None
     return (project, last_prjct_id)
@@ -77,12 +90,15 @@ def choose_project():
 
 @blueprint.route('/', methods=['GET', 'POST'])
 def rank_projects():
+    print('HHHEHEHHEJHWJE')
     project = choose_project()  # type: Projects
+    print('project', project)
     if not project:
         response = make_response(redirect(url_for('base')))
         response.set_cookie('error_message',
                             '<strong>You have already ranked all the projects</strong>', max_age=60)
         return response
+    print('Hate him', project)
     first_project, new_last_id = project
     response = make_response(render_template('rank_project.html', project=first_project.tojson()))
     response.set_cookie('last_project_id', str(new_last_id))
